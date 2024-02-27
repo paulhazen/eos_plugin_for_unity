b784231f8dce1f7a80e2bc18b55ab469a3834163
Paul Hazen <paul-hazen@live.com>
2024-02-27 14:15:03 -0800

    changes that don't trigger a copyright update.

--- Assets/Plugins/Source/Core/EOSManager.cs
+++ Assets/Plugins/Source/Core/EOSManager.cs
@@ -43,6 +43,7 @@
 // this is a sample change (testing it to see if it works)
 // this is ANOTHER change
 // YET ANOTHER change.
+// This change should not cause the copyright updater to update any files.
 // If using a 1.12 or newer, this allows the eos manager to use the new
 // bindings API to hook up and load the library
 #if EOS_DYNAMIC_BINDINGS
