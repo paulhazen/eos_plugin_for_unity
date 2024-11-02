60d6d57c40b01987feb58bb7fd9754522df8e5c9
Paul Hazen <paul-hazen@live.com>
2024-11-02 15:02:41 -0700

    fix: Move contents of framework.h into pch.h.

--- lib/NativeCode/DynamicLibraryLoaderHelper/NativeRender/pch.h
+++ lib/NativeCode/DynamicLibraryLoaderHelper/NativeRender/pch.h
@@ -8,7 +8,9 @@
 #define PCH_H
 
 // add headers that you want to pre-compile here
-#include "framework.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
 
 
 #if _WIN32 || _WIN64
