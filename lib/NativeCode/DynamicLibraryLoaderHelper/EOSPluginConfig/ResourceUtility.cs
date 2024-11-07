3fe5bdd16521fad5f38628b2e8aba7b1bc8e199b
Paul Hazen <paul-hazen@live.com>
2024-11-07 14:31:04 -0800

    fix: Add documentation to new ResourceUtility class.

--- lib/NativeCode/DynamicLibraryLoaderHelper/EOSPluginConfig/ResourceUtility.cs
+++ lib/NativeCode/DynamicLibraryLoaderHelper/EOSPluginConfig/ResourceUtility.cs
@@ -27,20 +27,45 @@ namespace PlayEveryWare.EpicOnlineServices
 {
     using Newtonsoft.Json;
 
+    /// <summary>
+    /// This class is used to read values from an embedded "config.json" file.
+    /// </summary>
     public static class ResourceUtility
     {
+        /// <summary>
+        /// Private readonly instance of the resource values.
+        /// </summary>
         private static readonly EmbeddedResourceValues s_values;
 
+        /// <summary>
+        /// Kept internal because there are a very limited set of circumstances
+        /// where these values need to be used.
+        /// </summary>
         internal class EmbeddedResourceValues
         {
             public string StreamingAssetsPath { get; set; }
         }
 
+        /// <summary>
+        /// Static constructor so that the static values can be set properly.
+        /// </summary>
         static ResourceUtility()
         {
             s_values = GetEmbeddedResourceValues();
         }
 
+        /// <summary>
+        /// Reads an embedded resource file of the given name.
+        /// </summary>
+        /// <param name="resourceFileName">
+        /// The name of the embedded file to read from.
+        /// </param>
+        /// <returns>
+        /// The string contents of the indicated embedded resource file.
+        /// </returns>
+        /// <exception cref="FileNotFoundException">
+        /// Thrown if the embedded file is not found.
+        /// </exception>
         private static string ReadEmbeddedJsonFile(string resourceFileName)
         {
             var assembly = Assembly.GetExecutingAssembly();
@@ -48,24 +73,34 @@ namespace PlayEveryWare.EpicOnlineServices
             // Automatically prepend the assembly name to the resource path
             string fullResourceName = $"EOSPluginConfig.{resourceFileName}";
 
-            using (Stream stream = assembly.GetManifestResourceStream(fullResourceName))
-            {
-                if (stream == null)
-                    throw new FileNotFoundException($"Resource not found: {fullResourceName}");
+            using Stream stream = assembly.GetManifestResourceStream(fullResourceName);
 
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    return reader.ReadToEnd();
-                }
-            }
+            // Deal with the circumstance where the file does not exist.
+            if (stream == null)
+                throw new FileNotFoundException($"Resource not found: {fullResourceName}");
+
+            using StreamReader reader = new(stream);
+            return reader.ReadToEnd();
         }
 
+        /// <summary>
+        /// Gets the embedded resource values.
+        /// </summary>
+        /// <returns>
+        /// The embedded resources contained within config.json.
+        /// </returns>
         private static EmbeddedResourceValues GetEmbeddedResourceValues()
         {
             string jsonContent = ReadEmbeddedJsonFile("config.json");
             return JsonConvert.DeserializeObject<EmbeddedResourceValues>(jsonContent);
         }
 
+        /// <summary>
+        /// Gets the streaming assets path.
+        /// </summary>
+        /// <returns>
+        /// Fully-qualified path to the streaming assets path.
+        /// </returns>
         public static string GetStreamingAssetsPath()
         {
             return s_values.StreamingAssetsPath;
