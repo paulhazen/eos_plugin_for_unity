6d82f4a57e63de0cb74b95ba611751836c3c9965
Paul Hazen <paul-hazen@live.com>
2024-11-26 11:58:15 -0800

    fix: Add comment header and header guard to PEW_EOS_Defines.h file.

--- lib/NativeCode/DynamicLibraryLoaderHelper/NativeRender/include/PEW_EOS_Defines.h
+++ lib/NativeCode/DynamicLibraryLoaderHelper/NativeRender/include/PEW_EOS_Defines.h
@@ -1,3 +1,29 @@
+#ifndef PEW_EOS_DEFINES_H
+#define PEW_EOS_DEFINES_H
+
+/*
+ * Copyright (c) 2021 PlayEveryWare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+#pragma once
+
 // Define PEW_EOS_EXPORT for exporting/importing symbols
 #ifdef _WIN32
 // PEW_EOS_EXPORT is defined *only* in the pre-processing scripting directives
@@ -27,3 +53,5 @@
  * external to the DLL.
  */
 #define PEW_EOS_API_FUNC(return_value) extern "C" PEW_EOS_API return_value __stdcall
+
+#endif
