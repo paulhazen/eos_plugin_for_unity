7e6f464cc28b9cef8d91805a4caa3721fd2c8add
Paul Hazen <paul-hazen@live.com>
2024-11-26 11:08:04 -0800

    fix: Add comments to PEW_EOS_Defines.h.

--- lib/NativeCode/DynamicLibraryLoaderHelper/NativeRender/include/PEW_EOS_Defines.h
+++ lib/NativeCode/DynamicLibraryLoaderHelper/NativeRender/include/PEW_EOS_Defines.h
@@ -1,6 +1,15 @@
 #pragma once
 // Define PEW_EOS_EXPORT for exporting/importing symbols
 #ifdef _WIN32
+// PEW_EOS_EXPORT is defined *only* in the pre-processing scripting directives
+// for GfxPluginNativeRender's build process. When PEW_EOS_EXPORT is defined,
+// using the PEW_EOS_API macro in class or struct definitions will affect the
+// export of those objects. However, when other projects reference any of the
+// files within this project - the PEW_EOS_EXPORT define will not be defined,
+// and (as is appropriate for the context in which another project is
+// referencing this one) the macro will specify that the struct or class should
+// be *imported*, not *exported*.
+// This is standard practice for C++ DLL projects.
 #ifdef PEW_EOS_EXPORT
 #define PEW_EOS_API __declspec(dllexport)
 #else
@@ -10,13 +19,10 @@
 #define PEW_EOS_API // Empty for non-Windows platforms
 #endif
 
-// Define a macro for exported functions with calling convention
+/**
+ * \brief Use this macro on the signature of a method that is to be exposed to
+ * callers external to the dll. Used exclusively for static methods.
+ * \param return_value Syntax to affect the exposure of the method to callers
+ * external to the DLL.
+ */
 #define PEW_EOS_API_FUNC(return_value) extern "C" PEW_EOS_API return_value __stdcall
-
-#define STRINGIFY(x) #x
-#define TO_STRING(x) STRINGIFY(x)
-
-// Strip surrounding quotes
-#define REMOVE_QUOTES(x) x[0] == '\"' ? (x + 1) : x
-#define STRIP_QUOTES(s) (REMOVE_QUOTES(s)[strlen(REMOVE_QUOTES(s)) - 1] == '\"' ? REMOVE_QUOTES(s) : s)
-
