b3988bdfa1120ac923f55f9b41dc5c0d73d993dd
Paul Hazen <paul-hazen@live.com>
2024-11-26 17:06:46 -0800

    fix: Increase coverage of documentation.

--- com.playeveryware.eos/Runtime/Core/Utility/ConfigurationUtility.cs
+++ com.playeveryware.eos/Runtime/Core/Utility/ConfigurationUtility.cs
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2024 PlayEveryWare
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 namespace PlayEveryWare.EpicOnlineServices.Utility
 {
     using Epic.OnlineServices;
@@ -10,9 +32,25 @@ namespace PlayEveryWare.EpicOnlineServices.Utility
     using UnityEngine;
 #endif
 
+    /// <summary>
+    /// This class is used to segregate the code responsible for taking
+    /// configuration values (as stored in JSON and editable by the user from
+    /// within the Unity editor) and converting those values into the data
+    /// structures that the EOS SDK requires to be created and initialized.
+    ///
+    /// The primary purpose of doing this - aside from separating the concerns,
+    /// is to allow efficient testing of the verisimilitude between the native
+    /// and managed implementations of this process.
+    /// </summary>
     public static class ConfigurationUtility
     {
-
+        /// <summary>
+        /// Get the create options used to make the EOS platform.
+        /// </summary>
+        /// <returns>
+        /// An object that encapsulates the CreateOptions used for creating the
+        /// EOS SDK platform.
+        /// </returns>
         public static EOSCreateOptions GetEOSCreateOptions()
         {
             PlatformConfig platformConfig = PlatformManager.GetPlatformConfig();
@@ -86,11 +124,17 @@ namespace PlayEveryWare.EpicOnlineServices.Utility
 #if !EXTERNAL_TO_UNITY
             platformSpecifics.ConfigureSystemPlatformCreateOptions(ref platformOptions);
 #endif
-
             return platformOptions;
         }
 
-
+        /// <summary>
+        /// Get the initialize options used to initialize the EOS SDK after it
+        /// has been created.
+        /// </summary>
+        /// <returns>
+        /// An object that encapsulates the initialization options utilized to
+        /// initialize the EOS SDK after it has been created.
+        /// </returns>
         public static EOSInitializeOptions GetEOSInitializeOptions()
         {
             EOSInitializeOptions initOptions = new() { options = new() };
@@ -116,6 +160,5 @@ namespace PlayEveryWare.EpicOnlineServices.Utility
             // Return;
             return initOptions;
         }
-
     }
 }
