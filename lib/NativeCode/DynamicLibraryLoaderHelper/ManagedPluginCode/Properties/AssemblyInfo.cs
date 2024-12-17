d0eaa954b194146bfc2441c5ad63e7e705535e00
Paul Hazen <paul-hazen@live.com>
2024-12-17 11:08:35 -0800

    fix: Add assembly info files to the managed (or mixed) class libraries.

--- lib/NativeCode/DynamicLibraryLoaderHelper/ManagedPluginCode/Properties/AssemblyInfo.cs
+++ lib/NativeCode/DynamicLibraryLoaderHelper/ManagedPluginCode/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("PlayEveryWare, Inc.")]
 [assembly: AssemblyProduct("EOS Plugin for Unity")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("en-US")]
 
